b95d7f5b32681fad536b5b07a800705139c3c535
joelm11 <j-jolabola@live.com>
2023-12-15 13:26:53 -0700

    Starting work on parsing cmdline arguments

---
+++ src/cmdline/cmdline.cpp
@@ -0,0 +1,7 @@
+#include "cmdline.hpp"
+#include <iostream>
+
+void util::parse_args() {
+    std::cout << "Parse args\n";
+    return;
+}
---
+++ src/cmdline/cmdline.hpp
@@ -0,0 +1,5 @@
+/* Utility functions for basic parsing of commandline arguments */
+namespace util {
+    // Parse input argument for chosen effect and modification factor
+    void parse_args();
+}
