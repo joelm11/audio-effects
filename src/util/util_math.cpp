147ae30665edbc8d23f62f29131fa5e49b236e39
joelm11 <j-jolabola@live.com>
2023-12-18 13:30:45 -0700

    Fixed window function

--- src/util/util_math.cpp
+++ src/util/util_math.cpp
@@ -1,14 +1,16 @@
 #include "util_math.hpp"
 #include <cmath>
+#include <iostream>
 #define _USE_MATH_DEFINES
 #include <math.h>
 
 void compute_hann_win(double *window_buffer, int win_size, int an_hop_size) {
     const double a = 0.54, b = -0.46; 
-    const double c_norm = std::sqrt(an_hop_size / win_size) / std::sqrt(4 * a * a + 2 * b * b);
+    const double c_norm = std::sqrt((double)an_hop_size / win_size) / std::sqrt(4 * a * a + 2 * b * b);
     const double pi_win = M_PI / win_size;
+    std::cout << c_norm << '\n';
     for (int i = 0; i < win_size; ++i) {
-        window_buffer[i] = a + b * std::cos(2 * M_PI * i) / win_size + pi_win;
+        window_buffer[i] = a + b * std::cos(2 * M_PI * i / win_size)  + pi_win;
         window_buffer[i] *= c_norm;
     }
 }
