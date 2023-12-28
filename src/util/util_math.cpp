1f9de40e1059d39f60314837a0de6368fae28cec
joelm11 <j-jolabola@live.com>
2023-12-28 10:31:39 -0700

    Verified window calculation and application

--- src/util/util_math.cpp
+++ src/util/util_math.cpp
@@ -3,17 +3,15 @@
 #include <iostream>
 #define _USE_MATH_DEFINES
 #include <math.h>
+#include <numbers>
 
 void compute_hann_win(double *window_buffer, int win_size, int an_hop_size) {
-    // const double a = 0.54, b = -0.46; 
-    // const double c_norm = std::sqrt((double)an_hop_size / win_size) / std::sqrt(4 * a * a + 2 * b * b);
-    // const double pi_win = M_PI / win_size;
-    // for (int i = 0; i < win_size; ++i) {
-    //     window_buffer[i] = a + b * std::cos(2 * M_PI * i / win_size)  + pi_win;
-    //     window_buffer[i] *= c_norm;
-    // }
+    const double a = 0.54, b = -0.46; 
+    const double c_norm = std::sqrt((double)an_hop_size / win_size) / std::sqrt(4 * a * a + 2 * b * b);
+    const double pi_win = M_PI / win_size;
     for (int i = 0; i < win_size; ++i) {
-        window_buffer[i] = std::pow(std::sin(M_PI * double (i) / win_size), 2);
+        window_buffer[i] = a + b * std::cos(2 * std::numbers::pi * i / win_size)  + pi_win;
+        window_buffer[i] *= c_norm;
     }
 }
 
